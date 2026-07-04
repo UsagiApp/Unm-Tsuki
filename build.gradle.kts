da45a865da78821d6422829a317bd3b6d33f230b
dragonx943 <premieregirl26@gmail.com>
2026-07-04 21:28:08 +0700

    Update mvn

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/UsagiApp/Tsuki")
+            url = uri("https://maven.pkg.github.com")
             credentials {
                 username = System.getenv("GITHUB_ACTOR") ?: ""
                 password = System.getenv("GITHUB_TOKEN") ?: ""
